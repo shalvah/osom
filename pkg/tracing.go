981bd05ce8bb851eec75463e40a5da4782e9488e
Shalvah <diakon.ng@gmail.com>
2025-12-05 00:53:25 +0100

    Tracing

--- pkg/tracing.go
+++ pkg/tracing.go
@@ -3,8 +3,10 @@ package config
 import (
 	"context"
 	"errors"
+	"log/slog"
 	"time"
 
+	"github.com/go-logr/logr"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
@@ -17,12 +19,14 @@ import (
 // SetupOTelSDK bootstraps the OpenTelemetry pipeline.
 // If it does not return an error, make sure to call shutdown for proper cleanup.
 func SetupOTelSDK(ctx context.Context) (func(context.Context) error, error) {
+	// Enable debug logging for OpenTelemetry SDK.
+	otel.SetLogger(logr.FromSlogHandler(slog.Default().Handler()))
+
 	var shutdownFuncs []func(context.Context) error
 	var err error
 
-	// shutdown calls cleanup functions registered via shutdownFuncs.
-	// The errors from the calls are joined.
-	// Each registered cleanup will be invoked once.
+	// shutdown calls all cleanup functions in the shutdownFuncs slice.
+	// And returns all errors from all calls.
 	shutdown := func(ctx context.Context) error {
 		var err error
 		for _, fn := range shutdownFuncs {
@@ -32,7 +36,7 @@ func SetupOTelSDK(ctx context.Context) (func(context.Context) error, error) {
 		return err
 	}
 
-	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	// handleErr calls shutdown to cleanup, then returns the err and any err from shutdown.
 	handleErr := func(inErr error) {
 		err = errors.Join(inErr, shutdown(ctx))
 	}
